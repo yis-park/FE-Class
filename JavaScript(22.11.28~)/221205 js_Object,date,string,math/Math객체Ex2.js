82cc887b07311384969d9c00ca6d270676066f5d
yis-park <ckadltmf423@naver.com>
2022-12-06 17:57:47 +0900

    1206 array 배열

--- JavaScript(22.11.28~)/221205 js_Object,date,string,math/Math객체Ex2.js
+++ JavaScript(22.11.28~)/221205 js_Object,date,string,math/Math객체Ex2.js
@@ -8,6 +8,17 @@ const btn = document.querySelector("#btn"); //제일최신거
 // function bgColor() { } 선언함수
 const bgColor = () => {
   //이아이로 쓰기
+  // 랜덤 컬러(명도, 채도의 % 그대로 두고 색상의 값을 랜덤으로)
+  // 0~359까지의 임의의 수
+  let ran = Math.trunc(Math.random() * 360);
+  console.log(ran);
+  let ranColor1 = `hsl(${ran},83%, 12% )`;
+  let ranColor2 = `hsl(${ran + 40}, 86%, 50%)`;
+
+  // setProperty('속성','값'); css 속성 제어
+
+  document.body.style.setProperty("--bg1", ranColor1);
+  document.body.style.setProperty("--bg2", ranColor2);
 };
 
 btn.addEventListener("click", bgColor); //콜백함수로 사용
